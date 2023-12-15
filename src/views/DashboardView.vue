6d469a069cff41ba1f8792f6845276e03b76c489
theravenmasque <theravenmasque@gmail.com>
2023-12-15 20:20:23 +0100

    side-navbar

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,142 +1,49 @@
 <template>
-  <nav class="dash-nav">
-    <div class="dashboard">
-      <div class="container-fluid">
-        <div class="d-flex flex-row">
-          <div class="d-flex bg-transparent px-sm-2 px-0">
-            <div
-              class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100"
-            >
-              <img
-                src="../assets/img/logos/fitness-logo-and-gym-icon-design-illustrationicon-free-vector.png"
-                height="35"
-                class="mb-3"
-              />
-              <ul
-                class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start"
-                id="menu"
-              >
-                <li class="nav-item">
-                  <router-link
-                    class="icons first p-1 text-decoration-none"
-                    to="/"
-                    ><img
-                      src="../assets/img/logos/home-icon.png"
-                      height="30"
-                      alt=""
-                  /></router-link>
-                </li>
-                <li>
-                  <a
-                    href="#submenu1"
-                    data-bs-toggle="collapse"
-                    class="nav-link px-0 align-middle"
-                  >
-                    <i class="fs-4 bi-speedometer2"></i>
-                    <span class="ms-1 d-none d-sm-inline"
-                      ><img
-                        src="../assets/img/logos/training.png"
-                        height="30"
-                        alt=""
-                    /></span>
-                  </a>
-                  <ul
-                    class="collapse show nav flex-column ms-1"
-                    id="submenu1"
-                    data-bs-parent="#menu"
-                  >
-                    <li class="w-100">
-                      <a href="#" class="nav-link px-0">
-                        <span class="d-none d-sm-inline"
-                          ><img
-                            src="../assets/img/logos/analitics.png"
-                            height="30"
-                            alt=""
-                        /></span>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#" class="nav-link px-0">
-                        <span class="d-none d-sm-inline"
-                          ><img
-                            src="../assets/img/logos/nutritions.png"
-                            height="30"
-                            alt=""
-                        /></span>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#" class="nav-link px-0">
-                        <span class="d-none d-sm-inline"
-                          ><img
-                            src="../assets/img/logos/log-out.png"
-                            height="30"
-                            alt=""
-                        /></span>
-                      </a>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-              <hr />
-            </div>
-          </div>
-          <div
-            class="content-card bg-transparent d-flex flex-column align-items-center mt-1"
-          >
-            <div
-              class="heading-dashboard d-flex flex-row justify-content-between w-100 mt-2"
-            >
-              <h5 class="text-white">Dashboard</h5>
-              <h6 class="text-white">
-                John Doe
-                <img
-                  class="ms-1"
-                  src="../assets/img/logos/avatar.png"
-                  height="40"
-                  alt=""
-                />
-              </h6>
-            </div>
-            <div
-              class="add-day d-flex flex-row justify-content-center border border-dark-3 align-items-center text-center mb-3"
-            >
-              <h5 class="text-white ps-2 pe-1">Plan Your Exercise</h5>
-              <button
-                type="button"
-                class="plus-btn hover-none p-1"
-              >
-                <img
-                  src="../assets/img/logos/add-icon.png"
-                  height="40"
-                  alt="add-button"
-                />
-              </button>
-              
-            </div>
-            <div
-              class="dashboard-content d-flex flex-row justify-content-center"
-            >
-             <AnalyticsCard/>
-             <ProgressCard/>
-            </div>
-          </div>
-        </div>
-      </div>
+  <div class="dashboard-wrapper d-flex flex-row">
+    <SideNavbar/>
+  <div
+    class="content-card bg-transparent d-flex flex-column justify-content-between"
+  >
+    <div
+      class="heading-dashboard d-flex flex-row justify-content-between me-5 ms-5 mt-2"
+    >
+      <h5 class="text-white">Dashboard</h5>
+      <h6 class="text-white">
+        John Doe
+        <img
+          class="ms-1"
+          src="../assets/img/logos/avatar.png"
+          height="40"
+          alt=""
+        />
+      </h6>
     </div>
-  </nav>
+    <div
+      class="add-day d-flex flex-row justify-content-center border border-dark-3 align-items-center align-self-center text-center mb-3"
+    >
+      <h5 class="text-white ps-2 pe-1">Plan Your Exercise</h5>
+      <button type="button" class="plus-btn hover-none p-1">
+        <img
+          src="../assets/img/logos/add-icon.png"
+          height="40"
+          alt="add-button"
+        />
+      </button>
+    </div>
+    <div class="dashboard-content d-flex flex-row justify-content-center"></div>
+  </div>
+</div>
 </template>
 
 <script lang="ts">
-import AnalyticsCard from "../components/analytics-component/AnalyticsCard.vue"
-import ProgressCard from "../components/progress-component/ProgressCard.vue"
-import { defineComponent} from "vue";
-
+// import AnalyticsCard from "../components/analytics-component/AnalyticsCard.vue"
+// import ProgressCard from "../components/progress-component/ProgressCard.vue"
+import { defineComponent } from "vue";
+import SideNavbar from "@/components/side-navbar/SideNavbar.vue";
 
 export default defineComponent({
   name: "DashboardView",
-  components: {AnalyticsCard, ProgressCard},
-  
+  components: {SideNavbar},
 });
 </script>
 <style lang="scss" scoped>
