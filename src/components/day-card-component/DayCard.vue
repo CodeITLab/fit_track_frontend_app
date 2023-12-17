0e238c3b6b90cdeeaf2b724225c1a6c956116f0d
theravenmasque <theravenmasque@gmail.com>
2023-12-17 16:19:04 +0100

    modal-counter

--- src/components/day-card-component/DayCard.vue
+++ src/components/day-card-component/DayCard.vue
@@ -5,6 +5,7 @@ export default defineComponent({
   data() {
     return {
       modalShow: false,
+      showInputField: false,
     };
   },
   methods: {
@@ -13,6 +14,12 @@ export default defineComponent({
     },
     close() {
       this.modalShow = false;
+    },
+    openInputField() {
+      this.showInputField = true;
+    },
+    closeInputField() {
+      this.showInputField = false;
     },
   },
 });
@@ -48,18 +55,73 @@ export default defineComponent({
           <h5 class="modal-title" id="exampleModalLabel">Start Your Planing</h5>
         </div>
         <div class="modal-body d-flex flex-column">
-          <form action="">
-            <label for="Name your Ex">Name your Ex</label>
+          <div class="d-flex flex-column">
+            <label for="Name your Ex">Name your Exercise</label>
             <input class="m-1" type="text" placeholder="" />
-            <!-- <label for="Day 1">Day one</label>
-            <input class="m-1" type="text" placeholder="" />
-            <input class="m-1" type="text" placeholder="" />
-            <input class="m-1" type="text" placeholder="" /> -->
-          </form>
-          <button>Add</button>
-          <input class="m-1" type="text" placeholder="" />
-        </div>
-        <div class="modal-footer">
+          </div>
+          <button
+            @click="openInputField()"
+            class="btn btn-primary w-40 align-self-center mt-2"
+          >
+            Add
+          </button>
+          <div
+            v-if="showInputField"
+            class="d-flex flex-column justify-content-center align-items-center"
+          >
+          <div class="d-flex justify-content-center align-items-center">
+              <input class="m-1" type="text" />
+              <div
+                class="counter d-flex flex-row justify-content-center align-items-center bg-primary"
+              >
+                <div
+                  class="d-flex justify-content-center align-items-center text-center me-2"
+                >
+                  <p class="m-0 text-center">0</p>
+                </div>
+                <div class="d-flex flex-column">
+                  <p class="border-0 btn m-0 p-0">+</p>
+                  <p class="border-0 btn mb-1 p-0">-</p>
+                </div>
+              </div>
+            </div>
+            <div class="d-flex justify-content-center align-items-center">
+              <input class="m-1" type="text" />
+              <div
+                class="counter d-flex flex-row justify-content-center align-items-center bg-primary"
+              >
+                <div
+                  class="d-flex justify-content-center align-items-center text-center me-2"
+                >
+                  <p class="m-0 text-center">0</p>
+                </div>
+                <div class="d-flex flex-column">
+                  <p class="border-0 btn m-0 p-0">+</p>
+                  <p class="border-0 btn mb-1 p-0">-</p>
+                </div>
+              </div>
+            </div>
+            <div class="d-flex justify-content-center align-items-center">
+              <input class="m-1" type="text" />
+              <div
+                class="counter d-flex flex-row justify-content-center align-items-center bg-primary"
+              >
+                <div
+                  class="d-flex justify-content-center align-items-center text-center me-2"
+                >
+                  <p class="m-0 text-center">0</p>
+                </div>
+                <div class="d-flex flex-column justify-content-center align-items-center">
+                  <button class="border-0  m-0 p-0">+</button>
+                  <button class="border-0  mb-1 p-0">-</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          </div>
+        <div
+          class="modal-footer d-flex justify-content-center align-items-center"
+        >
           <button
             @click="close()"
             type="button"
