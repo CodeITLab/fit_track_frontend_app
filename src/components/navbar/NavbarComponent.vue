0123fd843a7356d82d21623218e25997ac9d1b53
KristijanFištrek <fistrekkristijan@gmail.com>
2023-12-19 11:36:53 +0100

    Add: redesigned a main menu bar

--- src/components/navbar/NavbarComponent.vue
+++ src/components/navbar/NavbarComponent.vue
@@ -2,56 +2,37 @@
 </script>
 
 <template>
-  <div class="container-fluid">
-  <div class="collapse" id="navbarToggleExternalContent">
-    <div class="bg-transparent p-4">
-      <h5 class="text-white h4">Collapsed content</h5>
-      <span class="text-muted">Toggleable via the navbar brand.</span>
-    </div>
-  </div>
-  <nav class="navbar navbar-dark d-flex flex-row justify-content-between mt-2">
-    <div class="logo ms-3">
-      <img
-        v-bind:src="require('../../assets/img/logos/logo-proto.png')"
-        alt=""
-      />
-    </div>
-    <div class=" d-none d-lg-flex d-flex justify-content-center rounded-3 border border-dark-3">
-      <router-link class="links first p-1 text-decoration-none" to="/"
-        >Home</router-link
-      >
-      |
-      <router-link class="links p-1 text-decoration-none" to="/dashboard"
-        >Services</router-link
-      >
-      |
-      <router-link class="links p-1 text-decoration-none" to="/"
-        >Pricing</router-link
-      >
-      |
-      <router-link class="links p-1 text-decoration-none" to="/dashboard"
-        >Why Us</router-link
-      >
-      |
-      <router-link class="links last p-1 text-decoration-none" to="/"
-        >Our Work</router-link
-      >
-    </div>
-    <div class="me-3">
-      <button
-        class="navbar-toggler navbar-light"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarToggleExternalContent"
-        aria-controls="navbarToggleExternalContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
+  <nav class="navbar navbar-expand-lg">
+    <div class="container-fluid">
+      <button class="navbar-toggler" type="button" 
+        data-bs-toggle="collapse" 
+        data-bs-target="#main-menu"
+        aria-controls="main-menu" 
+        aria-expanded="false" 
+        aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
+      <div class="collapse navbar-collapse" id="main-menu">
+        <div class="logo ms-3">
+          <img v-bind:src="require('../../assets/img/logos/logo-proto.png')" alt="" />
+        </div>
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-link class="nav-link links first p-1 text-decoration-none active" to="/"
+        >Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link links p-1 text-decoration-none" to="/dashboard"
+        >Services</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link links p-1 text-decoration-none" to="/"
+        >Contact Us</router-link>
+          </li>
+        </ul>
+      </div>
     </div>
   </nav>
-</div>
 </template>
 
 <style lang="scss" scoped>
