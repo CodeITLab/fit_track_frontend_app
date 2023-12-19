dcbfd109ed196938c80ce07e3181f7184d040233
KristijanFištrek <fistrekkristijan@gmail.com>
2023-12-19 14:52:52 +0100

    Add: implemented eslint rules in configuration file

--- src/components/navbar/NavbarComponent.vue
+++ src/components/navbar/NavbarComponent.vue
@@ -1,20 +1,17 @@
 <script lang="ts" setup>
+
 </script>
 
 <template>
   <nav class="navbar navbar-expand-lg">
     <div class="container-fluid">
-      <button class="navbar-toggler" type="button" 
-        data-bs-toggle="collapse" 
-        data-bs-target="#main-menu"
-        aria-controls="main-menu" 
-        aria-expanded="false" 
-        aria-label="Toggle navigation">
-          <div class="main-menu-toggler">
-            <span></span>
-            <span></span>
-            <span></span>
-          </div>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-menu"
+        aria-controls="main-menu" aria-expanded="false" aria-label="Toggle navigation">
+        <div class="main-menu-toggler">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
       </button>
       <div class="collapse navbar-collapse" id="main-menu">
         <div class="logo ms-3">
@@ -22,16 +19,13 @@
         </div>
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <router-link class="nav-link links first p-1 text-decoration-none active" to="/"
-        >Home</router-link>
+            <router-link class="nav-link links first p-1 text-decoration-none active" to="/">Home</router-link>
           </li>
           <li class="nav-item">
-            <router-link class="nav-link links p-1 text-decoration-none" to="/dashboard"
-        >Services</router-link>
+            <router-link class="nav-link links p-1 text-decoration-none" to="/dashboard">Services</router-link>
           </li>
           <li class="nav-item">
-            <router-link class="nav-link links p-1 text-decoration-none" to="/"
-        >Contact Us</router-link>
+            <router-link class="nav-link links p-1 text-decoration-none" to="/">Contact Us</router-link>
           </li>
         </ul>
       </div>
