7085de5967297f939805bca690b5b0ca0cbb4355
KristijanFištrek <fistrekkristijan@gmail.com>
2023-12-21 17:38:44 +0100

    Added additional design to the table

--- src/components/create-workout-component/create-workout-modal/CreateWorkoutModalBody.vue
+++ src/components/create-workout-component/create-workout-modal/CreateWorkoutModalBody.vue
@@ -56,7 +56,8 @@ export default defineComponent({
     <div class="modal-body d-flex flex-column">
         <div class="d-flex flex-column">
             <label for="Name your Exercise">Name your Workout</label>
-            <input v-model="workoutData.workoutName" class="m-1" type="text" placeholder="Push day..." />
+            <input v-model="workoutData.workoutName" class="m-1" type="text"
+                   placeholder="Push day..." />
         </div>
         <hr />
         <div class="d-flex flex-column justify-content-center align-items-center">
@@ -65,46 +66,41 @@ export default defineComponent({
                 <table class="table-responsive">
                     <thead>
                         <th scope="col">Exercise Name</th>
-                        <th scope="col">Reps</th>
-                        <th scope="col">Sets</th>
+                        <th class="align-center" scope="col">Reps</th>
+                        <th class="align-center" scope="col">Sets</th>
                     </thead>
                     <tbody>
-                        <tr v-if="workoutData.exerciseData.length > 0" v-for="(value, index) in workoutData.exerciseData">
+                        <tr v-if="workoutData.exerciseData.length > 0"
+                            v-for="(value, index) in workoutData.exerciseData">
                             <td>
-                                <input v-model="value.exerciseName" :key="index" class="exercise-name-input" type="text"
-                                    placeholder="Push ups..." />
+                                <input v-model="value.exerciseName" :key="index"
+                                       class="exercise-name-input" type="text"
+                                       placeholder="Push ups..." />
                             </td>
                             <td>
                                 <div class="modal-button-group">
                                     <button @click="increaseNumberOfReps(index)"
-                                        class="counter-button border-0 bg-transparent ms-1 p-0">+</button>
-                                    <div class="d-flex justify-content-center align-items-center text-center m-2">
-                                        <p class="m-0 text-center" :key="index">{{ value.reps }}</p>
-                                    </div>
+                                            class="counter-button border-0 bg-transparent ms-1 p-0">+</button>
+                                    <p class="counter-value" :key="index">{{ value.reps }}</p>
                                     <button @click="decreaseNumberOfReps(index)"
-                                        class="counter-button border-0 bg-transparent me-1 p-0">-</button>
+                                            class="counter-button border-0 bg-transparent me-1 p-0">-</button>
                                 </div>
                             </td>
                             <td>
                                 <div class="modal-button-group">
                                     <button @click="increaseNumberOfSets(index)"
-                                        class="counter-button border-0 bg-transparent ms-1 p-0">+</button>
-                                    <div class="d-flex justify-content-center align-items-center text-center m-2">
-                                        <p class="m-0 text-center" :key="index">{{ value.sets }}</p>
-                                    </div>
+                                            class="counter-button border-0 bg-transparent ms-1 p-0">+</button>
+                                    <p class="counter-value" :key="index">{{ value.sets }}</p>
                                     <button @click="decreaseNumberOfSets(index)"
-                                        class="counter-button border-0 bg-transparent me-1 p-0">-</button>
+                                            class="counter-button border-0 bg-transparent me-1 p-0">-</button>
                                 </div>
-                            </td>
-                            <td>
-
                             </td>
                         </tr>
                         <tr>
-                            <td>
+                            <td colspan="3">
                                 <div class="footer-button-wrapper">
                                     <button @click="increaseNumberOfExercises()"
-                                        class="btn btn-primary align-self-center mt-2">
+                                            class="btn btn-primary align-self-center mt-2">
                                         Add Exercise
                                     </button>
                                 </div>
@@ -112,12 +108,14 @@ export default defineComponent({
                         </tr>
                         <tr>
                             <td>
-                                <div class="modal-footer d-flex justify-content-center align-items-center">
-                                    <button @click="setModalValue(false)" type="button" class="btn btn-secondary"
-                                        data-dismiss="modal">
+                                <div
+                                     class="modal-footer d-flex justify-content-center align-items-center">
+                                    <button @click="setModalValue(false)" type="button"
+                                            class="btn btn-secondary" data-dismiss="modal">
                                         Close
                                     </button>
-                                    <button type="button" class="btn btn-primary" @click="saveWorkoutData()">
+                                    <button type="button" class="btn btn-primary"
+                                            @click="saveWorkoutData()">
                                         Save changes
                                     </button>
                                 </div>
@@ -131,6 +129,4 @@ export default defineComponent({
     </div>
 </template>
 
-<style lang="css">
-@import "../../../assets/css/components/create-workout-modal/create-workout-modal-body.css";
-</style>
+<style lang="css">@import "../../../assets/css/components/create-workout-modal/create-workout-modal-body.css";</style>
