222970ccfca3f9422d17af8b3c9858f3bc661792
theravenmasque <theravenmasque@gmail.com>
2023-12-21 19:11:38 +0100

    nekaporuka

--- src/components/create-workout-component/create-workout-modal/CreateWorkoutModalBody.vue
+++ src/components/create-workout-component/create-workout-modal/CreateWorkoutModalBody.vue
@@ -57,109 +57,19 @@ export default defineComponent({
       store.createWorkout(this.workoutData);
     },
   },
-  mounted() {
-    console.log(this.workoutData);
-    const store = useWorkoutDataStore();
-    console.log(store.workouts, "g");
-  },
 });
 </script>
 
 <template>
-    <div class="modal-body d-flex flex-column">
-        <div class="d-flex flex-column">
-            <label for="Name your Exercise">Name your Workout</label>
-            <input v-model="workoutData.workoutName"
-                   class="m-1"
-                   type="text"
-                   placeholder="Push day..." />
-        </div>
-        <hr />
-        <div class="d-flex flex-column justify-content-center align-items-center">
-
-            <form action="submit"
-                  @submit.prevent="saveWorkoutData">
-                <table class="table-responsive">
-                    <thead>
-                        <th scope="col">Exercise Name</th>
-                        <th class="text-center"
-                            scope="col">Reps</th>
-                        <th class="text-center"
-                            scope="col">Sets</th>
-                    </thead>
-                    <tbody>
-                        <tr v-if="workoutData.exerciseData.length > 0"
-                            v-for="(value, index) in workoutData.exerciseData">
-                            <td>
-                                <input v-model="value.exerciseName"
-                                       :key="index"
-                                       class="exercise-name-input"
-                                       type="text"
-                                       placeholder="Push ups..." />
-                            </td>
-                            <td>
-                                <div class="modal-button-group">
-                                    <button @click="increaseNumberOfReps(index)"
-                                            class="counter-button border-0 bg-transparent ms-1 p-0">+</button>
-                                    <p class="counter-value"
-                                       :key="index">{{ value.reps }}</p>
-                                    <button @click="decreaseNumberOfReps(index)"
-                                            class="counter-button border-0 bg-transparent me-1 p-0">-</button>
-                                </div>
-                            </td>
-                            <td>
-                                <div class="modal-button-group">
-                                    <button @click="increaseNumberOfSets(index)"
-                                            class="counter-button border-0 bg-transparent ms-1 p-0">+</button>
-                                    <p class="counter-value"
-                                       :key="index">{{ value.sets }}</p>
-                                    <button @click="decreaseNumberOfSets(index)"
-                                            class="counter-button border-0 bg-transparent me-1 p-0">-</button>
-                                </div>
-                            </td>
-                            <td>
-                                <button @click="removeExercise(index)"
-                                        type="button"
-                                        class="delete-btn">
-                                    <img height="16"
-                                         src="../../../assets/img/icons/delete.png"
-                                         alt="delete button">
-                                </button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td colspan="4">
-                                <div class="footer-button-wrapper">
-                                    <button @click="increaseNumberOfExercises()"
-                                            class="btn btn-primary align-self-center mt-2">
-                                        Add Exercise
-                                    </button>
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td colspan="4">
-                                <div
-                                     class="modal-footer d-flex justify-content-center align-items-center">
-                                    <button @click="setModalValue(false)"
-                                            type="button"
-                                            class="btn btn-secondary"
-                                            data-dismiss="modal">
-                                        Close
-                                    </button>
-                                    <button type="button"
-                                            class="btn btn-primary"
-                                            @click="saveWorkoutData()">
-                                        Save changes
-                                    </button>
-                                </div>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </form>
-        </div>
-
+  <div class="modal-body d-flex flex-column">
+    <div class="d-flex flex-column">
+      <label for="Name your Exercise">Name your Workout</label>
+      <input
+        v-model="workoutData.workoutName"
+        class="m-1"
+        type="text"
+        placeholder="Push day..."
+      />
     </div>
     <hr />
     <div class="d-flex flex-column justify-content-center align-items-center">
@@ -167,10 +77,11 @@ export default defineComponent({
         <table class="table-responsive">
           <thead>
             <th scope="col">Exercise Name</th>
-            <th class="align-center" scope="col">Reps</th>
-            <th class="align-center" scope="col">Sets</th>
+            <th class="text-center" scope="col">Reps</th>
+            <th class="text-center" scope="col">Sets</th>
           </thead>
           <tbody>
+            <!-- eslint-disable vue/no-use-v-if-with-v-for,vue/no-confusing-v-for-v-if -->
             <tr
               v-if="workoutData.exerciseData.length > 0"
               v-for="(value, index) in workoutData.exerciseData"
