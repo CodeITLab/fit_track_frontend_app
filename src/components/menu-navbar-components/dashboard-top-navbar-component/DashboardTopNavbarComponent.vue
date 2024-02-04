1c732a1d494e730aa3ec70acd4bfbcf419684653
theravenmasque <theravenmasque@gmail.com>
2024-02-04 17:45:24 +0100

    Update DashboardTopNavbarComponent.vue

--- src/components/menu-navbar-components/dashboard-top-navbar-component/DashboardTopNavbarComponent.vue
+++ src/components/menu-navbar-components/dashboard-top-navbar-component/DashboardTopNavbarComponent.vue
@@ -1,11 +1,11 @@
 <script lang="ts" setup>
-import {UserDataControler} from "../../../controllers/user-controllers/UserControler";
+import { UserDataControler } from "../../../controllers/user-controllers/UserControler";
 import { StoreAccessController } from "@/controllers/store-access/StoreAccessController";
 import { reactive } from "vue";
 import { login } from "@/services/GoogleLogins";
 
 const getWorkoutData = reactive(
-  StoreAccessController().userStore.getUserInfo
+    StoreAccessController().userStore.getUserInfo
 );
 </script>
 <template>
@@ -13,10 +13,10 @@ const getWorkoutData = reactive(
          class="content-card bg-transparent d-flex flex-column justify-content-between position-fixed top">
         <div class="heading-dashboard d-flex flex-row justify-content-between me-5 ms-5 mt-3">
             <h5 class="text-white">Dashboard</h5>
-            <h6 class="text-white" >
-                {{getWorkoutData.name}}
+            <h6 class="text-white">
+                {{ getWorkoutData.name }}
                 <img class="border border-2 border-dark rounded-circle ms-1"
-                     :src= getWorkoutData.picture
+                     :src=getWorkoutData.picture
                      height="45"
                      alt="" />
             </h6>
