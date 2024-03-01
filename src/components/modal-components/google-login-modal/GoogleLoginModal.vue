0f7b898a5d25b341fa96a470ce316bd353b4faf2
kristijanfistrek <fistrekkristijan@gmail.com>
2024-03-01 16:59:21 +0100

    Add: login redesigned

--- src/components/modal-components/google-login-modal/GoogleLoginModal.vue
+++ src/components/modal-components/google-login-modal/GoogleLoginModal.vue
@@ -8,27 +8,41 @@
                     <h5 class="modal-title" id="exampleModalLabel">Login</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <div class="modal-body">
-                    <div class="login-form">
-                        <div class="form-floating mb-3">
-                            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                            <label for="floatingInput">Email address</label>
+                <div class="modal-body w-100">
+                    <div class="row">
+                        <div class="col">
+                            <p>Enter Your Credentials</p>
+                            <div class="login-form">
+                                <div class="form-floating mb-3">
+                                    <input type="email" class="form-control" id="floatingInput"
+                                        placeholder="name@example.com">
+                                    <label for="floatingInput">Email address</label>
+                                </div>
+                                <div class="form-floating">
+                                    <input type="password" class="form-control" id="floatingPassword"
+                                        placeholder="Password">
+                                    <label for="floatingPassword">Password</label>
+                                </div>
+                            </div>
+                            <div class="additional-login-info mt-4">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                                    <label class="form-check-label" for="flexCheckDefault">
+                                        Remember me
+                                    </label>
+                                </div>
+                            </div>
                         </div>
-                        <div class="form-floating">
-                            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-                            <label for="floatingPassword">Password</label>
+                        <div class="col">
+                            <p>Login With Social Media</p>
+                            <div class="social-media-login">
+                                <button type="button" class="btn btn-outline-danger">
+                                    <a href="#" class="google btn">
+                                        <i class="fa fa-google fa-fw"></i> Login with Google
+                                    </a>
+                                </button>
+                            </div>
                         </div>
-                    </div>
-                    <div class="additional-login-info mt-4">
-                        <div class="form-check">
-                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-                            <label class="form-check-label" for="flexCheckDefault">
-                                Remember me
-                            </label>
-                        </div>
-                    </div>
-                    <div class="social-media-login">
-
                     </div>
                 </div>
                 <div class="modal-footer w-100">
