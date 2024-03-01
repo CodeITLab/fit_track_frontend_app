fdb0174fa73f6d4a016add4f10679c511d030865
KristijanFištrek <fistrekkristijan@gmail.com>
2024-03-01 14:31:21 +0100

    Add: added new router settings

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,45 +1,17 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import DashboardView from '../views/DashboardView.vue'
-import { UserAuthController } from '@/controllers/auth-controllers/UserAuthController';
-import { login } from '@/services/GoogleLogins';
-import { useUserDataStore } from '@/store/UserData';
-import { StoreAccessController } from '@/controllers/store-access/StoreAccessController';
-import { ModalController } from "@/controllers/modal-controllers/ModalController";
-import { GetUserData } from '@/services/GetUserData';
 
 const routes: Array<RouteRecordRaw> = [
-    {
-        path: '/',
-        name: 'home',
-        component: HomeView
-    },
-    {
-        path: '/dashboard',
-        name: 'dashboard',
-        meta: { roles: ['admin'] },
-        beforeEnter: (to, from, next) => {
-       
-          const authFlag = localStorage.getItem('isAuth');
-        
-          if(from.path === '/' && authFlag !== null) {
-            next();
-          } else {
-            ModalController().setModalComponentModalValue(true)
-            next('/');
-          } 
-        },
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: DashboardView
-    // () => import(/* webpackChunkName: "about" */ '../views/DashboardView.vue')
-    }
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  }
 ]
 
 const router = createRouter({
-    history: createWebHistory(process.env.BASE_URL),
-    routes
+  history: createWebHistory(process.env.BASE_URL),
+  routes
 })
 
 export default router
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,26 @@
-<script setup>
+<script lang="ts" setup>
+
+import { onBeforeMount, ref } from 'vue';
+import { useFetch } from '@/api/useFetch';
+import { IUser } from '@/models/IUser';
+
+const userData = ref<IUser | null>();
+const userDataErrors = ref(false);
+const userID = 0;
+
+onBeforeMount(async () => {
+  const { data, hasError } = await useFetch<IUser>(
+    'http://127.0.0.1:8080/user/get-user-data?id=' + userID
+  );
+
+  userData.value = data.value;
+  userDataErrors.value = hasError.value;
+});
 
 </script>
 
-<template></template>
+<template>
+  <p>{{ userData?.name }}</p>
+</template>
 
 <style></style>
