acdb8e9f863eb99d80f1c3a33e9654e7a4a28eec
kristijanfistrek <fistrekkristijan@gmail.com>
2024-03-02 22:05:17 +0100

    Add: added logout component

---
+++ src/components/modal-components/logout-modal/LogoutModal.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div class="modal-wrapper">
+        <div class="modal-wrapper-header"></div>
+        <div class="modal-wrapper-body"></div>
+        <div class="modal-wrapper-footer"></div>
+    </div>
+</template>
+
+<style></style>
