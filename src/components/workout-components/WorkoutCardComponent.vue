af085e3e771deaf4bf7635b8c9bd46dd5f9f3eb8
kristijanfistrek <fistrekkristijan@gmail.com>
2024-03-06 23:00:35 +0100

    Add: refactore for create your workout

--- src/components/workout-components/WorkoutCardComponent.vue
+++ src/components/workout-components/WorkoutCardComponent.vue
@@ -1,44 +1,38 @@
-<script lang="ts"
-        setup>
-        import { getData } from '@/api/useFetch';
-        import { IWorkoutModel } from '@/models/IWorkoutModel';
-        import { useUserStore } from '@/store/userStore';
-        import { useModalStore } from '@/store/modalStore';
-        import { onBeforeMount, onMounted, ref } from 'vue';
+<script lang="ts" setup>
+import { getData } from '@/api/useFetch';
+import { IWorkoutModel } from '@/models/IWorkoutModel';
+import { useModalStore } from '@/store/modalStore';
+import { onBeforeMount, ref } from 'vue';
 
-        const workoutData = ref<IWorkoutModel[] | null>([]);
-        const workoutDataErrors = ref(false);
+const workoutData = ref<IWorkoutModel[] | null>([]);
+const workoutDataErrors = ref(false);
 
-        onBeforeMount(async () => {
 
-          const { data, hasError } = await getData<IWorkoutModel[]>(
-            'http://127.0.0.1:8080/workouts/get-users-workouts?email=' + localStorage.getItem('email')
-          );
+onBeforeMount(async () => {
 
-          workoutData.value = data.value;
-          workoutDataErrors.value = hasError.value;
-          if (data.value!.length > 1) {
-            useModalStore().setCreateWorkoutModalValue(false)
-          }
-        });
+  const { data, hasError } = await getData<IWorkoutModel[]>(
+    'http://127.0.0.1:8080/workouts/get-users-workouts?email=' + localStorage.getItem('email')
+  );
+
+  workoutData.value = data.value;
+  workoutDataErrors.value = hasError.value;
+  if (data.value!.length > 1) {
+    useModalStore().setIsPlanYourWorkoutModalActive(false)
+  }
+});
 </script>
 
 <template>
   <div class="card-wrapper container-fluid ">
-    <div v-for="item in workoutData"
-         class="cards bg-light shadow "
-         style="--bs-bg-opacity: 0.3">
+    <div v-for="item in workoutData" class="cards bg-light shadow " style="--bs-bg-opacity: 0.3">
       <h3 class="font text-white">{{ item.name }}</h3>
     </div>
 
   </div>
 
-
-
   <p v-for="item in workoutData">{{ item.name }}</p>
 </template>
 
-<style lang="css"
-       scoped>
-      @import "../../assets/css/components/workout-components/workout-card-component.css"
-    </style>
+<style lang="css" scoped>
+@import "../../assets/css/components/workout-components/workout-card-component.css"
+</style>
