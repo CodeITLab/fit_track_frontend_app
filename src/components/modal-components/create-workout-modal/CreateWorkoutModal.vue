ff4c6ae6cffa30981f88d615bdebce611dfaf9bb
kristijanfistrek <fistrekkristijan@gmail.com>
2024-03-06 23:39:37 +0100

    Add: added workout modal

--- src/components/modal-components/create-workout-modal/CreateWorkoutModal.vue
+++ src/components/modal-components/create-workout-modal/CreateWorkoutModal.vue
@@ -1,14 +1,20 @@
 <script lang="ts" setup>
 import { useModalStore } from '@/store/modalStore';
 
+const closeModal = () => {
+    useModalStore().setCreateYourWorkoutModalValue(false);
+    useModalStore().setIsPlanYourWorkoutModalActive(true);
+}
+
 </script>
 
 <template>
-    <div class="container-fluid" v-if="useModalStore().getCreateWorkoutModalValue">
-        <div class="create-workout-modal">
-            <p>Ovo je workout modal.</p>
-        </div>
+    <div class="container-fluid create-workout-modal" v-if="useModalStore().getCreateWorkoutModalValue">
+        <p>Ovo je workout modal.</p>
+        <button @click="closeModal">Close</button>
     </div>
 </template>
 
-<style></style>
+<style lang="css" scoped>
+@import "/src/assets/css/components/modals-components/create-workout-modal/create-workout-modal.css"
+</style>
