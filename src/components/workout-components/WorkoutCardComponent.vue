2f032bce3e791d8ed057f7942edb06090c2e9b64
theravenmasque <theravenmasque@gmail.com>
2024-03-06 16:03:30 +0100

    changes

--- src/components/workout-components/WorkoutCardComponent.vue
+++ src/components/workout-components/WorkoutCardComponent.vue
@@ -1,26 +1,44 @@
-<script lang="ts" setup>
-import { getData } from '@/api/useFetch';
-import { IWorkoutModel } from '@/models/IWorkoutModel';
-import { useUserStore } from '@/store/userStore';
-import { onBeforeMount, ref } from 'vue';
+<script lang="ts"
+        setup>
+        import { getData } from '@/api/useFetch';
+        import { IWorkoutModel } from '@/models/IWorkoutModel';
+        import { useUserStore } from '@/store/userStore';
+        import { useModalStore } from '@/store/modalStore';
+        import { onBeforeMount, onMounted, ref } from 'vue';
 
-const workoutData = ref<IWorkoutModel[] | null>([]);
-const workoutDataErrors = ref(false);
+        const workoutData = ref<IWorkoutModel[] | null>([]);
+        const workoutDataErrors = ref(false);
 
-onBeforeMount(async () => {
+        onBeforeMount(async () => {
 
-const { data, hasError } = await getData<IWorkoutModel[]>(
-  'http://127.0.0.1:8080/workouts/get-users-workouts?email=' + localStorage.getItem('email')
-);
+          const { data, hasError } = await getData<IWorkoutModel[]>(
+            'http://127.0.0.1:8080/workouts/get-users-workouts?email=' + localStorage.getItem('email')
+          );
 
-workoutData.value = data.value;
-workoutDataErrors.value = hasError.value;
-});
-
+          workoutData.value = data.value;
+          workoutDataErrors.value = hasError.value;
+          if (data.value!.length > 1) {
+            useModalStore().setCreateWorkoutModalValue(false)
+          }
+        });
 </script>
 
 <template>
-    <p v-for="item in workoutData">{{ item.name }}</p>
+  <div class="card-wrapper container-fluid ">
+    <div v-for="item in workoutData"
+         class="cards bg-light shadow "
+         style="--bs-bg-opacity: 0.3">
+      <h3 class="font text-white">{{ item.name }}</h3>
+    </div>
+
+  </div>
+
+
+
+  <p v-for="item in workoutData">{{ item.name }}</p>
 </template>
 
-<style></style>
+<style lang="css"
+       scoped>
+      @import "../../assets/css/components/workout-components/workout-card-component.css"
+    </style>
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,11 +1,12 @@
-<script lang="ts" setup>
+<script lang="ts"
+        setup>
 
-import TopNavbarComponent from '@/components/menu-components/TopNavbarComponent.vue';
-import SideNavbarComponent from '@/components/menu-components/SideNavbarComponent.vue';
-import LogoutModal from '@/components/modal-components/logout-modal/LogoutModal.vue';
-import CreateWorkoutButton from '@/components/create-workout/CreateWorkoutButton.vue';
-import CreateWorkoutModal from '@/components/modal-components/create-workout-modal/CreateWorkoutModal.vue';
-import WorkoutCardComponent from '@/components/workout-components/WorkoutCardComponent.vue';
+        import TopNavbarComponent from '@/components/menu-components/TopNavbarComponent.vue';
+        import SideNavbarComponent from '@/components/menu-components/SideNavbarComponent.vue';
+        import LogoutModal from '@/components/modal-components/logout-modal/LogoutModal.vue';
+        import CreateWorkoutButton from '@/components/create-workout/CreateWorkoutButton.vue';
+        import CreateWorkoutModal from '@/components/modal-components/create-workout-modal/CreateWorkoutModal.vue';
+        import WorkoutCardComponent from '@/components/workout-components/WorkoutCardComponent.vue';
 
 </script>
 
@@ -16,7 +17,9 @@ import WorkoutCardComponent from '@/components/workout-components/WorkoutCardCom
     </aside>
     <main>
       <TopNavbarComponent />
+
       <WorkoutCardComponent />
+
       <CreateWorkoutButton />
       <CreateWorkoutModal />
       <LogoutModal />
@@ -25,6 +28,7 @@ import WorkoutCardComponent from '@/components/workout-components/WorkoutCardCom
 </template>
 
 
-<style lang="scss" scoped>
-@import "../assets/css/views/dashboard.css";
-</style>
+<style lang="scss"
+       scoped>
+      @import "../assets/css/views/dashboard.css";
+    </style>
