e4fba2216a53b3034c8108838fe8402a29473497
kristijanfistrek <fistrekkristijan@gmail.com>
2024-03-07 23:41:35 +0100

    Add: added form kit into the project

--- src/components/modal-components/create-workout-modal/CreateWorkoutModal.vue
+++ src/components/modal-components/create-workout-modal/CreateWorkoutModal.vue
@@ -1,24 +1,30 @@
-<script lang="ts"
-        setup>
-        import { useModalStore } from '@/store/modalStore';
+<script lang="ts" setup>
+import { useModalStore } from '@/store/modalStore';
+import { ref } from 'vue';
 
-        const closeModal = () => {
-            useModalStore().setCreateYourWorkoutModalValue(false);
-            // zbog linije ispod se otvaral Plan Your Exercise button kad sam stisnul close na Ovo je workout Modal, zakomentiral sam ju samo za svaki slučaj. Ali mislim kolko sad vidim na prvu da nam ne treba tu.
-            // useModalStore().setIsPlanYourWorkoutModalActive(true);
-        }
+const submitted = ref(false)
+
+const closeModal = () => {
+    useModalStore().setCreateYourWorkoutModalValue(false);
+    // zbog linije ispod se otvaral Plan Your Exercise button kad sam stisnul close na Ovo je workout Modal, zakomentiral sam ju samo za svaki slučaj. Ali mislim kolko sad vidim na prvu da nam ne treba tu.
+    // useModalStore().setIsPlanYourWorkoutModalActive(true);
+}
+
+const submit = (values: string) => {
+    console.log(values)
+}
 
 </script>
 
 <template>
-    <div class="container-fluid create-workout-modal"
-         v-if="useModalStore().getCreateWorkoutModalValue">
-        <p>Ovo je workout modal.</p>
+    <div class="container-fluid create-workout-modal" v-if="useModalStore().getCreateWorkoutModalValue">
+        <FormKit type="form" submit-label="Create Your Workout" @submit="submit">
+            <FormKit name="workoutName" label="Workout Name" validation="required" />
+        </FormKit>
         <button @click="closeModal">Close</button>
     </div>
 </template>
 
-<style lang="css"
-       scoped>
-    @import "/src/assets/css/components/modals-components/create-workout-modal/create-workout-modal.css"
+<style lang="css" scoped>
+@import "/src/assets/css/components/modals-components/create-workout-modal/create-workout-modal.css"
 </style>
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+// @ts-ignore
+import { plugin, defaultConfig } from '@formkit/vue'
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -10,5 +13,5 @@ const pinia = createPinia();
 const app = createApp(App)
 
 app.use(pinia)
-app.use(router).mount('#app')
+app.use(router).use(plugin, defaultConfig).mount('#app')
 
