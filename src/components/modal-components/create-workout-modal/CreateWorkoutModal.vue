a9500fd4f52d694c3fd3d1085071a5d079338def
theravenmasque <theravenmasque@gmail.com>
2024-03-07 11:20:48 +0100

    plus button

--- src/components/modal-components/create-workout-modal/CreateWorkoutModal.vue
+++ src/components/modal-components/create-workout-modal/CreateWorkoutModal.vue
@@ -1,20 +1,24 @@
-<script lang="ts" setup>
-import { useModalStore } from '@/store/modalStore';
+<script lang="ts"
+        setup>
+        import { useModalStore } from '@/store/modalStore';
 
-const closeModal = () => {
-    useModalStore().setCreateYourWorkoutModalValue(false);
-    useModalStore().setIsPlanYourWorkoutModalActive(true);
-}
+        const closeModal = () => {
+            useModalStore().setCreateYourWorkoutModalValue(false);
+            // zbog linije ispod se otvaral Plan Your Exercise button kad sam stisnul close na Ovo je workout Modal, zakomentiral sam ju samo za svaki slučaj. Ali mislim kolko sad vidim na prvu da nam ne treba tu.
+            useModalStore().setIsPlanYourWorkoutModalActive(true);
+        }
 
 </script>
 
 <template>
-    <div class="container-fluid create-workout-modal" v-if="useModalStore().getCreateWorkoutModalValue">
+    <div class="container-fluid create-workout-modal"
+         v-if="useModalStore().getCreateWorkoutModalValue">
         <p>Ovo je workout modal.</p>
         <button @click="closeModal">Close</button>
     </div>
 </template>
 
-<style lang="css" scoped>
-@import "/src/assets/css/components/modals-components/create-workout-modal/create-workout-modal.css"
+<style lang="css"
+       scoped>
+    @import "/src/assets/css/components/modals-components/create-workout-modal/create-workout-modal.css"
 </style>
--- src/components/workout-components/WorkoutCardComponent.vue
+++ src/components/workout-components/WorkoutCardComponent.vue
@@ -1,36 +1,48 @@
-<script lang="ts" setup>
-import { getData } from '@/api/useFetch';
-import { IWorkoutModel } from '@/models/IWorkoutModel';
-import { useModalStore } from '@/store/modalStore';
-import { onBeforeMount, ref } from 'vue';
+<script lang="ts"
+        setup>
+        import { getData } from '@/api/useFetch';
+        import { IWorkoutModel } from '@/models/IWorkoutModel';
+        import { useModalStore } from '@/store/modalStore';
+        import { onBeforeMount, ref } from 'vue';
 
-const workoutData = ref<IWorkoutModel[] | null>([]);
-const workoutDataErrors = ref(false);
+        const workoutData = ref<IWorkoutModel[] | null>([]);
+        const workoutDataErrors = ref(false);
 
 
-onBeforeMount(async () => {
+        onBeforeMount(async () => {
 
-  const { data, hasError } = await getData<IWorkoutModel[]>(
-    'http://127.0.0.1:8080/workouts/get-users-workouts?email=' + localStorage.getItem('email')
-  );
+          const { data, hasError } = await getData<IWorkoutModel[]>(
+            'http://127.0.0.1:8080/workouts/get-users-workouts?email=' + localStorage.getItem('email')
+          );
 
-  workoutData.value = data.value;
-  workoutDataErrors.value = hasError.value;
+          workoutData.value = data.value;
+          workoutDataErrors.value = hasError.value;
 
-  if (data.value) {
-    useModalStore().setIsPlanYourWorkoutModalActive(false)
-  }
-});
+          if (data.value) {
+            useModalStore().setIsPlanYourWorkoutModalActive(false)
+          }
+        });
 </script>
 
 <template>
   <div class="card-wrapper">
-    <div v-for="item in workoutData" class="cards">
+    <div v-for="item in workoutData"
+         class="cards">
       <h3 class="font text-white">{{ item.name }}</h3>
+    </div>
+    <div class="plus">
+      <button @click="useModalStore().setCreateYourWorkoutModalValue(true)"
+              type="button"
+              class="plus-btn">
+        <img src="../../assets/img/logos/add-icon.png"
+             height="40"
+             alt="add-button" />
+      </button>
     </div>
   </div>
 </template>
 
-<style lang="css" scoped>
-@import "../../assets/css/components/workout-components/workout-card-component.css"
-</style>
+<style lang="css"
+       scoped>
+      @import "../../assets/css/components/workout-components/workout-card-component.css"
+    </style>
