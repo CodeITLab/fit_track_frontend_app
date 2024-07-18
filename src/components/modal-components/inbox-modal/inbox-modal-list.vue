1d7f9063c46869301c28f058904a6f98f9cc2e48
kristijanfistrek <fistrekkristijan@gmail.com>
2024-07-18 16:21:54 +0200

    Add: added inbox modal component templates

---
+++ src/components/modal-components/inbox-modal/inbox-modal-list.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="inbox-modal">
+    <div class="inbox-modal-header"></div>
+    <div class="inbox-modal-footer"></div>
+  </div>
+</template>
+
+<style lang="css" scoped></style>
