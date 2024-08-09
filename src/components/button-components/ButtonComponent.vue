a6b16c5c72967dabbab2b781df05a6d2b853d6f3
Goran Novaković <goran1.novakovic@gmail.com>
2024-08-09 08:05:19 +0100

    Add: reusable-button

---
+++ src/components/button-components/ButtonComponent.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+defineProps({
+  backgroundColor: String,
+  borderColor: String,
+  btnText: String,
+});
+</script>
+
+<template>
+  <div class="container">
+    <button
+      class="btn text-white border border-dark-3 mt-3"
+      type="button"
+      :class="{ backgroundColor: backgroundColor, borderColor: borderColor }"
+    >
+      {{ btnText }}
+    </button>
+  </div>
+</template>
+<style lang="css">
+@import "../../assets/css/components/button-components/button-component.css";
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,11 +2,11 @@
 import LoginModal from "@/components/modal-components/login-modal/LoginModal.vue";
 import { HomePageLoginText } from "@/helpers/TextEnums";
 import ModalManager from "@/controllers/ModalManagerController";
+import ButtonComponent from "@/components/button-components/ButtonComponent.vue";
 
 const isLoginModalActive = (): boolean => {
   return ModalManager().GetCurrentModalValue()?.name === "googleLoginModal";
 };
-
 </script>
 
 <template>
@@ -18,17 +18,19 @@ const isLoginModalActive = (): boolean => {
             Fitness <span class="highlighted">And</span> Tracking
           </h1>
         </div>
-        <hr/>
+        <hr />
         <div class="main-content-description">
           <p class="text-white">
             Mi vjerujemo da svatko ima potencijal za transformaciju.
           </p>
           <p class="text-white">
-            Bez obzira jeste li tek početnik ili iskusni sportaš,
-            naša aplikacija je dizajnirana kako bi vam pomogla da postignete svoje ciljeve i osjetite snagu unutar sebe.
+            Bez obzira jeste li tek početnik ili iskusni sportaš, naša
+            aplikacija je dizajnirana kako bi vam pomogla da postignete svoje
+            ciljeve i osjetite snagu unutar sebe.
           </p>
           <p class="text-white">
-            Počnite danas i osjetite razliku! <br/> Zajedno, pretvaramo izazove u pobjede.
+            Počnite danas i osjetite razliku! <br />
+            Zajedno, pretvaramo izazove u pobjede.
           </p>
         </div>
         <div class="main-content-footer mt-4">
@@ -47,36 +49,55 @@ const isLoginModalActive = (): boolean => {
           >
             {{ HomePageLoginText.SIGN_UP }}
           </button>
+          <ButtonComponent>
+            backgroundColor: backgroundColor, borderColor: borderColor, btnText:
+            {{ KLIKNI ME }}
+          </ButtonComponent>
         </div>
       </div>
       <div class="col side-image">
-        <img width="460" src="../assets/img/backgrounds/fitness-homepage.jpg"
-             alt="Homepage Fitness Image">
+        <img
+          width="460"
+          src="../assets/img/backgrounds/fitness-homepage.jpg"
+          alt="Homepage Fitness Image"
+        />
       </div>
       <div class="login-modal" v-if="isLoginModalActive()">
         <LoginModal />
       </div>
     </div>
-    <hr/>
+    <hr />
     <div class="row p-4">
       <div class="col side-image">
-        <img width="720" src="../assets/img/backgrounds/app-dashboard.jpg" alt="App Dashboard">
+        <img
+          width="720"
+          src="../assets/img/backgrounds/app-dashboard.jpg"
+          alt="App Dashboard"
+        />
       </div>
       <div class="col">
         <div class="main-content-title">
-          <h3 class="text-white">Što je Fitness <span class="highlighted">And</span> Tracking?</h3>
+          <h3 class="text-white">
+            Što je Fitness <span class="highlighted">And</span> Tracking?
+          </h3>
         </div>
-        <hr/>
+        <hr />
         <div class="main-content-description">
           <p class="text-white">
-            Fitness platforma lagana za korištenje, bez obzira jeste li potpuni početnik ili iskusni vježbač.
-          </p> <br/>
+            Fitness platforma lagana za korištenje, bez obzira jeste li potpuni
+            početnik ili iskusni vježbač.
+          </p>
+          <br />
           <p class="text-white">
-            Kreirajte vlastite rutine, unosite vježbe, pratite analitiku napretka i učite o različitim vrstama vježbi i njihovoj pravilnoj izvedbi.
-          </p><br/>
+            Kreirajte vlastite rutine, unosite vježbe, pratite analitiku
+            napretka i učite o različitim vrstama vježbi i njihovoj pravilnoj
+            izvedbi.
+          </p>
+          <br />
           <p class="text-white">
-            Aplikacija je namijenjena i za trenere koji mogu kreirati grupne treninge,
-            pratiti napredak svojih klijenata i upravljati njihovim programima treninga na jednom mjestu.
+            Aplikacija je namijenjena i za trenere koji mogu kreirati grupne
+            treninge, pratiti napredak svojih klijenata i upravljati njihovim
+            programima treninga na jednom mjestu.
           </p>
         </div>
       </div>
